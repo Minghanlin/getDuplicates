29510e0e75c21951995f62d3245c012fc5c4e76a
Minghan Lin <cd>
2016-08-12 09:14:14 +0800

    throw error still wrong

--- app.spec.js
+++ app.spec.js
@@ -1,12 +1,12 @@
 describe('Checking getDuplicates function',function(){
-it('throws Error when detects non integer',function(){
-expect(getDuplicates([a, 3, d])).toThrowError(TypeError, "Not an integer");
-});
 it('Throws error for non array',function(){
 expect(getDuplicates('nonarray')).toThrowError(TypeError, "Not an Array");
 });
-it('accepts empty arrasy',function(){
-expect(getDuplicates([])).not.toBe(null);
+it('throws Error when detects non integer',function(){
+expect(getDuplicates([a, 3, d])).toThrowError(TypeError, "Not an integer");
+});
+it('accepts empty array',function(){
+expect(getDuplicates(emptyarr)).not.toBe(null);
 });
 it('returns empty array if no duplicates',function(){
 if(getDuplicates([1, 2, 3, 4])) {
